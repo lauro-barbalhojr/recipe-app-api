e09dbf62576a4e71b2a3aee8eccde0e50da346cb
Lauro <lauro.abj@gmail.com>
2022-06-05 22:26:45 -0300

    First correction on database setup

--- app/app/tests.py
+++ app/app/tests.py
@@ -12,7 +12,7 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         res = calc.add(5, 6)
 
-        self.assertEqual(res, 11 )
+        self.assertEqual(res, 11)
     
     def test_subtract_numbers(self):
         '''Test for subtracting numbers'''
